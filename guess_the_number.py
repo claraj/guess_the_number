720bb342dd8b1727916746948d774b721c6d1f13
xm6264jz <ahmed.abdinoor3@gmail.com>
2020-09-08 21:54:20 -0500

    added a counter guess

--- guess_the_number.py
+++ guess_the_number.py
@@ -39,15 +39,17 @@ def main():
 
     (low, high) = configure_range()
     secret = generate_secret(low, high)
-
+    counter = 0
     while True:
         guess = get_guess()
         result = check_guess(guess, secret)
+        counter = counter +1
         print(result)
 
         if result == correct:
             break
-
+    print(f'You guessed {counter} times')    
+    
 
 if __name__ == '__main__':
     main()
