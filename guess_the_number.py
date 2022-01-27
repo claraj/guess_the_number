b6b134388b6b597444532d95039fee139950856d
BaldEagleFacts <email>
2022-01-27 12:44:49 -0600

    Count the number of attempts it took to guess the random number and display that info to the user upon game completion.

--- guess_the_number.py
+++ guess_the_number.py
@@ -30,19 +30,28 @@ def check_guess(guess, secret):
         return too_high
 
 
-def main():
+def counter(count):
+    count += 1
+    return count
 
+
+def main():
     (low, high) = configure_range()
     secret = generate_secret(low, high)
+    count = 0
 
     while True:
         guess = get_guess()
         result = check_guess(guess, secret)
+        count = counter(count)
         print(result)
 
         if result == correct:
             break
-
+    if count == 1:
+        print('You got it first try!')
+    else:
+        print(f'Correct. You got it after {count} guesses.')
     print('Thanks for playing the game!')
 
 
