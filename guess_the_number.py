247dde3891a09d2d4fe02a3dcde77296b746e1f9
himoa <@blahblah>
2022-09-08 23:09:56 -0500

    I added a counter variable and while loop and try except validation

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,3 +1,10 @@
+""" 1- Once the user has guessed correctly, display the number of guesses it took
+
+Hint: add a counter variable in the main function and increment it when the while loop repeats. 
+
+    2- Handle ValueError if user enters input other than an integer.
+    https://github.com/claraj/ProgrammingLogic1150Examples/blob/c874241cb507ee3d84c8506de69ce106134f45b5/4_loops/while_loop_check_for_number.py#L47-L60 """
+
 import random
 
 correct = 'you guessed correctly!'
@@ -17,7 +24,12 @@ def generate_secret(low, high):
 
 def get_guess():
     """ get user's guess, as an integer number """
-    return int(input('Guess the secret number? '))
+    while True:
+        try:
+            user_entry = int(input('Guess the secret number? '))
+            return user_entry
+        except ValueError:
+            print('Please enter a valid number')
 
 
 def check_guess(guess, secret):
@@ -35,14 +47,18 @@ def main():
     (low, high) = configure_range()
     secret = generate_secret(low, high)
 
+    counter = 0
+    
     while True:
         guess = get_guess()
         result = check_guess(guess, secret)
+        counter = counter + 1
         print(result)
 
         if result == correct:
             break
 
+    print('It took you ' + str(int(counter)) + ' guesses.')
     print('Thanks for playing the game!')
 
 
