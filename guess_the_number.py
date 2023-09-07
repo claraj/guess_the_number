ae3afd05340d47e529bf665e56eea5dbc91d987c
Jay <jerry.sirisavath@go.minneapolis.edu>
2023-09-06 20:19:53 -0500

    Modified Range to be 1000 instead

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,27 +1,28 @@
 import random
 
-correct = 'you guessed correctly!'
-too_low = 'too low'
-too_high = 'too high'
+
+correct = "you guessed correctly!"
+too_low = "too low"
+too_high = "too high"
 
 
 def configure_range():
-    """ Set the high and low values for the random number """
-    return 1, 10
+    """Set the high and low values for the random number"""
+    return 1, 1000 # Change range to be 1000 instead of 10.
 
 
 def generate_secret(low, high):
-    """ Generate a secret number for the user to guess """
+    """Generate a secret number for the user to guess"""
     return random.randint(low, high)
 
 
 def get_guess():
-    """ get user's guess, as an integer number """
-    return int(input('Guess the secret number? '))
+    """get user's guess, as an integer number"""
+    return int(input("Guess the secret number? "))
 
 
 def check_guess(guess, secret):
-    """ compare guess and secret, return string describing result of comparison """
+    """compare guess and secret, return string describing result of comparison"""
     if guess == secret:
         return correct
     if guess < secret:
@@ -31,7 +32,6 @@ def check_guess(guess, secret):
 
 
 def main():
-
     (low, high) = configure_range()
     secret = generate_secret(low, high)
 
@@ -43,8 +43,8 @@ def main():
         if result == correct:
             break
 
-    print('Thanks for playing the game!')
+    print("Thanks for playing the game!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
