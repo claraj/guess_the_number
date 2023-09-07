fa7c625f6eda75e6b9b134fcce568e760483baa5
Jay <jerry.sirisavath@go.minneapolis.edu>
2023-09-07 15:50:30 -0500

    Added a feature to users and diplay total guess they made until the correct guess.Previously users would not know how many times they guess until the correct number.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,27 +1,27 @@
 import random
 
-correct = 'you guessed correctly!'
-too_low = 'too low'
-too_high = 'too high'
+correct = "you guessed correctly!"
+too_low = "too low"
+too_high = "too high"
 
 
 def configure_range():
-    """ Set the high and low values for the random number """
+    """Set the high and low values for the random number"""
     return 1, 10
 
 
 def generate_secret(low, high):
-    """ Generate a secret number for the user to guess """
+    """Generate a secret number for the user to guess"""
     return random.randint(low, high)
 
 
 def get_guess():
-    """ get user's guess, as an integer number """
-    return int(input('Guess the secret number? '))
+    """get user's guess, as an integer number"""
+    return int(input("Guess the secret number? "))
 
 
 def check_guess(guess, secret):
-    """ compare guess and secret, return string describing result of comparison """
+    """compare guess and secret, return string describing result of comparison"""
     if guess == secret:
         return correct
     if guess < secret:
@@ -31,20 +31,26 @@ def check_guess(guess, secret):
 
 
 def main():
-
     (low, high) = configure_range()
     secret = generate_secret(low, high)
+
+    # Total guess counter is initialize with 0 at first
+    count = 0
 
     while True:
         guess = get_guess()
         result = check_guess(guess, secret)
+        # Add 1 count to guess counter when users guess
+        count += 1
         print(result)
 
         if result == correct:
+            # If the guess is correct, show the counter
+            print(f"Total guess: {count}")
             break
 
-    print('Thanks for playing the game!')
+    print("Thanks for playing the game!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
