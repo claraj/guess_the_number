65b83021da012f12e0e233d997958682fbf3eaa2
SalvLav <richardjthies95@gmail.com>
2023-09-09 22:18:19 -0500

    Created conflict

--- guess_the_number.py
+++ guess_the_number.py
@@ -47,7 +47,7 @@ def main():
         if result == correct:
             break
 
-    print(f'Thanks for playing the game! It took you {guesses} guesses.')
+    print(f'Thank you user for playing! It took you {guesses} guesses.')
 
 
 if __name__ == '__main__':
