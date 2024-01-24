d4d03756cab72286ceb47bc8dc4a4b10920689a2
sndmnska <102878282+sndmnska@users.noreply.github.com>
2024-01-24 14:09:13 -0600

    Add counter for total # of guests

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,6 +5,7 @@ too_low = 'too low'
 too_high = 'too high'
 
 
+
 def configure_range():
     """ Set the high and low values for the random number """
     return 1, 10
@@ -14,6 +15,10 @@ def generate_secret(low, high):
     """ Generate a secret number for the user to guess """
     return random.randint(low, high)
 
+def increment_guess_counter(total_guesses):
+    """Increments the total guesses for each successive loop"""
+    total_guesses += 1
+    return total_guesses
 
 def get_guess():
     """ get user's guess, as an integer number """
@@ -31,11 +36,12 @@ def check_guess(guess, secret):
 
 
 def main():
-
+    total_guesses = 0
     (low, high) = configure_range()
     secret = generate_secret(low, high)
 
     while True:
+        total_guesses = increment_guess_counter(total_guesses)
         guess = get_guess()
         result = check_guess(guess, secret)
         print(result)
@@ -43,6 +49,7 @@ def main():
         if result == correct:
             break
 
+    print(f"It took you {total_guesses} guesses this time.")
     print('Thanks for playing the game!')
 
 
